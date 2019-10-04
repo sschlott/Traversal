00e4246b80da4560a95367dd26894ca104fa2350
sschlott <43354996+sschlott@users.noreply.github.com>
2019-10-04 14:04:39 -0700

    added comments, some bug fixing

--- traverse.py
+++ traverse.py
@@ -7,7 +7,9 @@ This program implements a test to find paths that traverse each edge exactly onc
 from graph import Graph
 
 def traverse():
-    """Tests the Hopcroft-Tarjan algorithm for finding articulation points."""
+    '''
+    Reads ina a graph from a text files and parses for eurlean paths
+    '''
     g = Graph()
     while True:
         filename = input("Enter name of graph file: ")
@@ -18,43 +20,73 @@ def traverse():
         g.clear()
         g.load(filename)   
         available = g.getArcs()
-        traverseAll(g, available,available[0], [])   
+        nodes = g.getNodes()
+        if checkIfEuler(g) == False:
+            print("No such path exists")
+        for starting_point in available:
+            newList=available.copy()
+            traverseAll(g, newList,starting_point, [])  
+
+def checkIfEuler(graph):
+    '''
+    Uses the notion "A graph has an Euler path if and only 
+    if there are at most two vertices with odd degree." to
+    return an error in cases of no path.
+    '''
+    i=0
+    for node in graph.getNodes():
+        if len(node.getArcsFrom())%2 == 1:
+            i+=1
+    if i >= 3:
+        return False
+    else:
+        return True
+
 
 def removeFullArc(graph,arc,unvisited):
-    # newList = unvisited.copy()
+    '''
+    Removes the full undirected arc from a given list
+    '''
     rev = removeReverseArc(graph,arc,unvisited)
     unvisited.remove(arc)
 
 
 def removeReverseArc(graph,arc,unvisited):
+    '''
+    Removes an arc going the opposing direction of the input
+    (helper for removing an undirected arc)
+    '''
     for rev in unvisited:
         if rev.getStart() == arc.getFinish() and rev.getFinish() == arc.getStart():
             unvisited.remove(rev)
             break;
 
 def traverseAll(graph,unvisited,start, path):
-    if path == []:
-        path.append(start)
-    elif path[-1].getFinish() == start.getStart():
-        path.append(start)
-    else: 
-        print("wah",path,start)
-        return []
-    if unvisited == []:
-        print("found a good path", path)
-        return path
-
+    '''
+    Creates a list of unvisited arcs and recursively calls itself
+    on a smaller list of unvisited. If it doesn't have any more
+    arcs that meet the condition of having a matching end point 
+    for the next arc to start from, it moves on.
+    '''
     removeFullArc(graph,start,unvisited)
-    newList=unvisited.copy()
-    for neighbor in start.getFinish().getArcsFrom():
-        if neighbor in unvisited:
-            traverseAll(graph, newList, neighbor,path)
-
-        
-
-
-
-
+    if unvisited == []:
+        if path[-1].getFinish() == start.getStart():
+            path.append(start)
+            print(path)
+            return path
+        else:
+            return []
+    #Checks if the last item's endpoint is the same as your start point's beginnign
+    if path == [] or path[-1].getFinish() == start.getStart():
+        path.append(start)
+        for neighbor in start.getFinish().getArcsFrom():
+            if neighbor in unvisited:
+                newList=unvisited.copy()
+                # print(neighbor, newList)
+                traverseAll(graph, newList, neighbor,path)
+    else:
+        return False
+    
 # Startup code
 
 if __name__ == "__main__":
